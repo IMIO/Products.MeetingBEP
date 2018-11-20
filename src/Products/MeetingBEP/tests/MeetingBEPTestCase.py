d68c37f1be8718d06c183486c72ed9140dffe3b2
Gauthier Bastien <g.bastien@imio.be>
2018-11-20 21:15:07 +0000

    Fixed adding orgs thru ToolInitializer

--- src/Products/MeetingBEP/tests/MeetingBEPTestCase.py
+++ src/Products/MeetingBEP/tests/MeetingBEPTestCase.py
@@ -21,13 +21,14 @@
 #
 
 
-from Products.PloneMeeting.exportimport.content import ToolInitializer
-from Products.PloneMeeting.model.adaptations import performWorkflowAdaptations
-from Products.PloneMeeting.tests.PloneMeetingTestCase import pm_logger
-from Products.MeetingCommunes.tests.MeetingCommunesTestCase import MeetingCommunesTestCase
+from Products.MeetingBEP.config import PROJECTNAME
 from Products.MeetingBEP.profiles.zbep.import_data import rhc_org
 from Products.MeetingBEP.testing import MBEP_TESTING_PROFILE_FUNCTIONAL
 from Products.MeetingBEP.tests.helpers import MeetingBEPTestingHelpers
+from Products.MeetingCommunes.tests.MeetingCommunesTestCase import MeetingCommunesTestCase
+from Products.PloneMeeting.exportimport.content import ToolInitializer
+from Products.PloneMeeting.model.adaptations import performWorkflowAdaptations
+from Products.PloneMeeting.tests.PloneMeetingTestCase import pm_logger
 
 
 class MeetingBEPTestCase(MeetingCommunesTestCase, MeetingBEPTestingHelpers):
@@ -46,7 +47,9 @@ class MeetingBEPTestCase(MeetingCommunesTestCase, MeetingBEPTestingHelpers):
     def setUpRestrictedPowerObservers(self):
         """"""
         self.changeUser('siteadmin')
-        self.tool.addUsersAndGroups(groups=[rhc_org])
+        context = self.portal.portal_setup._getImportContext('Products.MeetingBEP:testing')
+        initializer = ToolInitializer(context, PROJECTNAME)
+        initializer.addOrgs([rhc_org])
         cfg = self.meetingConfig
         cfg.setItemPowerObserversStates(('itemcreated', 'presented', 'returned_to_proposing_group',))
         cfg.setItemRestrictedPowerObserversStates(('itemcreated', 'presented', 'returned_to_proposing_group',))
